27fc2e08f17db89a27c0779af48747e48a7a7ca2
LL\ishmehta2 <isha.mehta@publicissapient.com>
2024-02-05 00:59:39 +0000

    AI Inventory

--- src/main/java/com/popeye/rest/webservices/restfulwebservices/model/request/ChatRequest.java
+++ src/main/java/com/popeye/rest/webservices/restfulwebservices/model/request/ChatRequest.java
@@ -1,14 +1,53 @@
 package com.popeye.rest.webservices.restfulwebservices.model.request;
 
+import java.util.List;
+
 public class ChatRequest {
 
-    private String chatRequestMessage;
+    private String message;
+    List<Integer> eventIds;
+    List<HotelInfo> hotelInfo;
+    private Integer noOfPerson;
+    private Integer count;
 
-    public String getChatRequestMessage() {
-        return chatRequestMessage;
+
+    public String getMessage() {
+        return message;
     }
 
-    public void setChatRequestMessage(String chatRequestMessage) {
-        this.chatRequestMessage = chatRequestMessage;
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Integer> getEventIds() {
+        return eventIds;
+    }
+
+    public void setEventIds(List<Integer> eventIds) {
+        this.eventIds = eventIds;
+    }
+
+    public List<HotelInfo> getHotelInfo() {
+        return hotelInfo;
+    }
+
+    public void setHotelInfo(List<HotelInfo> hotelInfo) {
+        this.hotelInfo = hotelInfo;
+    }
+
+    public Integer getNoOfPerson() {
+        return noOfPerson;
+    }
+
+    public void setNoOfPerson(Integer noOfPerson) {
+        this.noOfPerson = noOfPerson;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
     }
 }
---
+++ src/main/java/com/popeye/rest/webservices/restfulwebservices/model/request/HotelInfo.java
@@ -0,0 +1,34 @@
+package com.popeye.rest.webservices.restfulwebservices.model.request;
+
+import java.util.List;
+
+public class HotelInfo {
+
+    private Integer hotelId;
+    private String checkIn;
+    private String checkOut;
+
+    public Integer getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(String checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public String getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(String checkOut) {
+        this.checkOut = checkOut;
+    }
+}
